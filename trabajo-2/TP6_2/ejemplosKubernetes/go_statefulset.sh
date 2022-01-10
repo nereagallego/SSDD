1edcdedca1e3ddb5f3396d223091b0b76f261b06
801950 <801950@unizar.es>
2022-01-10 12:47:13 -0800

    p

--- trabajo-2/TP6_2/ejemplosKubernetes/go_statefulset.sh
+++ trabajo-2/TP6_2/ejemplosKubernetes/go_statefulset.sh
@@ -1,14 +1,5 @@
 kubectl delete -f statefulset_go.yaml
-kubectl delete pvc www
-kubectl delete pvc www-raft-0
-kubectl delete pvc www-raft-1
-kubectl delete pvc www-raft-2
-#kubectl delete statefulset raft
-#kubectl delete service almacen
 kubectl delete pod raft-0
 kubectl delete pod raft-1
 kubectl delete pod raft-2
-kubectl delete configmap cm-go
-kubectl create configmap cm-go --from-file=./fichero
 kubectl create -f statefulset_go.yaml
-#kubectl apply -f statefulset_go.yaml
